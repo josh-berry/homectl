3be261665293ed4fcac0affac17ecc647eef6f97
Joshua J. Berry <des@condordes.net>
2018-02-24 14:04:55 -0800

    shell-boot: Make sure homectl is available in non-login shells
    Check for and load the profile if the 'hc' command can't be found.

--- homectl.hcpkg/shell-boot/rc.sh
+++ homectl.hcpkg/shell-boot/rc.sh
@@ -1,2 +1,6 @@
-source ~/.homectl/common/shell-boot/functions.sh
+if which hc >/dev/null 2>/dev/null; then
+    source ~/.homectl/common/shell-boot/functions.sh
+else
+    source ~/.homectl/common/shell-boot/profile.sh
+fi
 homectl-run-hooks rc
