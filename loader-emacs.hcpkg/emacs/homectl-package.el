26357f334f38f4765fe077ff22ccfad657c81f30
Joshua J. Berry <des@condordes.net>
2020-04-04 17:19:29 -0700

    loader-emacs: Teach `homectl-package` about `package-selected-packages`
    There is a bug in `package-install` where `package-selected-packages` is
    not updated if it's empty.  We explicitly update it after installing a
    package, to ensure the user has a complete set of selected packages and
    `package` won't inadvertently uninstall something installed by
    `homectl-require`.
    
    We also explicitly save `package-selected-packages` to the user's
    custom-file, so that a difference in installed packages will be
    noticeable/is recorded correctly in the user's homectl setup (and can be
    committed to git along with the change that actually installed the
    package).

--- loader-emacs.hcpkg/emacs/homectl-package.el
+++ loader-emacs.hcpkg/emacs/homectl-package.el
@@ -21,16 +21,39 @@ not already present."
   "Loads packages, possibly installing them through package.el if necessary."
 
   (dolist (pkg pkg-symbols)
+    ;; Try to load the package first locally
     (unless (require pkg nil t)
+
+      ;; It wasn't found; refresh the package list if it hasn't been loaded yet
+      ;; this session
       (unless homectl-package-refreshed-recently-p
         (package-refresh-contents)
         (setq homectl-package-refreshed-recently-p t))
+
+      ;; Install and load the package
+      (message "[homectl] Installing package: %s" pkg)
       (package-install pkg)
-      (require pkg))))
+      (require pkg))
+
+    ;; If the package was installed by package.el at some point (now or in the
+    ;; past), explicitly mark it as user-selected.  (This explicit marking is
+    ;; necessary because of a bug in package.el where if the
+    ;; package-selected-packages list is empty, package.el thinks the package is
+    ;; already marked as user-selected.)
+    (when (not (package-built-in-p pkg))
+      (add-to-list 'package-selected-packages pkg))))
 
 
 
 ;; Add MELPA by default since that's where most packages come from.
 (homectl-package-add-repo "melpa" "https://melpa.org/packages/")
+
+;; After startup, make sure package-selected-packages is saved to custom.el so
+;; the user will notice the differences in their homectl setup.
+(defun homectl-package--save-selected-packages ()
+  (customize-save-variable 'package-selected-packages
+                           package-selected-packages))
+
+(add-to-list 'emacs-startup-hook #'homectl-package--save-selected-packages)
 
 (provide 'homectl-package)
