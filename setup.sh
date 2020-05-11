e6db7dd1aeb67ef4d51008cc69b5fd9314dcdf00
Joshua J. Berry <des@condordes.net>
2020-05-10 20:52:59 -0700

    Simplify setup.sh
    Let the `homectl` tool handle reporting usage/help.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 homectl="$(dirname "$0")/homectl.hcpkg/bin/hc"
-
-if [[ "$1" == -* ]] || [[ -z "$1" ]]; then
-    exec "$homectl" help init
-fi
-
 exec "$homectl" init "$@"
